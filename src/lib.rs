8ff9f9d00252bea4d2db2f7313bb5f3dcd30698b
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2025-07-21 21:10:36 -0400

    Support rustdoc v54 and use latest `cargo_metadata`. (#1372)

--- src/lib.rs
+++ src/lib.rs
@@ -553,7 +553,7 @@ note: skipped the following crates since they have no library target: {skipped}"
                                 overrides,
                                 current_crate_data: CrateDataForRustdoc {
                                     crate_type: rustdoc_gen::CrateType::Current,
-                                    name: crate_name.clone(),
+                                    name: crate_name.to_string(),
                                     registry_name: registry_name.map(From::from),
                                     feature_config: &self.current_feature_config,
                                     build_target: self.build_target.as_deref(),
@@ -562,7 +562,7 @@ note: skipped the following crates since they have no library target: {skipped}"
                                     crate_type: rustdoc_gen::CrateType::Baseline {
                                         highest_allowed_version: Some(version.clone()),
                                     },
-                                    name: crate_name.clone(),
+                                    name: crate_name.to_string(),
                                     registry_name: registry_name.map(From::from),
                                     feature_config: &self.baseline_feature_config,
                                     build_target: self.build_target.as_deref(),
